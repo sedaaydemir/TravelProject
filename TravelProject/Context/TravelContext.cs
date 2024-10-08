e6826891f5cea7962caea500814fdd4882fdd2ea
Seda Aydemir <seda.aydemir7@gmail.com>
2024-10-08 21:58:30 +0300

    Add project files.

---
+++ TravelProject/Context/TravelContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using TravelProject.Entities;
+
+namespace TravelProject.Context
+{
+    public class TravelContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
